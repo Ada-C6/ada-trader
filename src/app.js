15fead0469633892f1b1483d6b380c04b155b932
Sarah Nahmias <nahmisa@gmail.com>
2016-11-28 14:48:55 -0800

    Adds stock data hash

--- src/app.js
+++ src/app.js
@@ -2,6 +2,38 @@ import $ from 'jquery';
 import _ from 'underscore';
 import ApplicationView from 'app/views/application_view';
 
+
+var stockData = [
+  {
+    symbol: 'HUMOR',
+    price: 8850
+  }, {
+    symbol: 'CLOTH',
+    price: '8170'
+  }, {
+    symbol: 'HABIT',
+    price: 9980
+  }, {
+      symbol: 'SUPER',
+      price: 8310
+  }, {
+      symbol: 'INGRD',
+      price: 7940
+  }, {
+      symbol: 'MXTPE',
+      price: 10920
+  }, {
+      symbol: 'CNTAR',
+      price: 9070
+  }, {
+      symbol: 'EVCLR',
+      price: 10190
+  },  {
+      symbol: 'FUZZY',
+      price: 8860
+  }
+];
+
 const simulate = function(quote) {
   // Calculate a random price movement
   const maxChange = 1.00;
