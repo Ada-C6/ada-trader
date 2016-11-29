c8c95a7298d40beef3042505aa69ad3296b30d17
Emily Abrash <emabrash@gmail.com>
2016-11-28 16:32:16 -0800

    Wave 3 - buttons working

--- src/app/views/quote_view.js
+++ src/app/views/quote_view.js
@@ -1,5 +1,6 @@
 import Backbone from 'backbone';
 import _ from 'underscore';
+import $ from 'jquery';
 
 // We pass in an object to the function that contains multiple functions (initialize
 // and render)
@@ -12,6 +13,18 @@ var QuoteView = Backbone.View.extend({
   render: function(){
     this.$el.html(this.template({symbol: this.quote.symbol, price: this.quote.price}));
     return this;
+  },
+  events: {
+    'click .btn-buy': 'buyStock',
+    'click .btn-sell': 'sellStock'
+  },
+  buyStock: function(event){
+    this.quote.price += 1.00;
+    this.render();
+  },
+  sellStock: function(event){
+    this.quote.price -= 1.00;
+    this.render();
   }
 });
 
