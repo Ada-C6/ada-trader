8693baf9f7fca07f6c7a13aab7945452abd5f219
KellyPT <trankphuong@gmail.com>
2016-11-28 16:20:02 -0800

    completed initialize() and render() for Wave 2.

--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -10,22 +10,38 @@ const ApplicationView = Backbone.View.extend({
     this.quoteData = options.quoteData;
 
     //Compile the same Underscore template from Wave 1. This compiled template will replace the one used in `QuoteView` during Wave 1.
+    this.quoteTemplate = _.template($('#tmpl-quote-view').html());
 
     //Create and store a list of *at least* two `QuoteView` instances. Each instance should be given:
     //     * The data for a single quote.
     //     * The compiled Underscore template from the above step.
+    this.cardList = [];
+    this.quoteData.forEach(function(quote) {
+      var card = new QuoteView({
+        quote: quote,
+        template: this.quoteTemplate
+      });
+
+      this.cardList.push(card);
+    }, this);
 
     //Store the HTML unordered list element from `index.html` that will contain the list of rendered `quotes`.
+    this.listElement = this.$('.quotes');
   },
 
-
   // * Have a `render` function that should:
-  //   * Render each `QuoteView` instance in the list created within `initialize`.
-  //   * Use the stored HTML unordered list element.
-  //   * Append to that element the jQuery object for each `QuoteView` instance we rendered.
   render: function() {
-    this.$el.html('<p>Hello World</p>');
+    // this.$el.html('<p>Hello World</p>');
 
+    // Make sure the list in the DOM is empty before we start appending items
+    this.listElement.empty();
+    //   * Render each `QuoteView` instance in the list created within `initialize`.
+    //   * Use the stored HTML unordered list element.
+    //   * Append to that element the jQuery object for each `QuoteView` instance we rendered.
+    this.cardList.forEach(function(card) {
+      card.render();
+      this.listElement.append(card.$el);
+    }, this);
     return this;
   }
 });
