6ecabd5353da8b7b4ad15cacf85e2c5ada8b963f
Nina Mutty <ninamutty@gmail.com>
2016-11-28 16:11:38 -0800

    basic render and intialize methods complete for application

--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -1,14 +1,55 @@
-import Backbone from 'backbone';
+import $ from 'jquery';    // Letting us use jquery within this document
+import Backbone from 'backbone'; // importing backbone
+import _ from 'underscore';
+import QuoteView from 'app/views/quote_view';
 
 const ApplicationView = Backbone.View.extend({
-  initialize: function() {
-  },
+  initialize: function(options) {
+    // Store a the full list of tasks
+    this.stockData = options.stockData;
+
+    // Compile a template to be shared between the individual tasks
+    this.stockTemplate = _.template($('#tmpl-quote-view').html());
+
+    // Keep track of the <ul> element
+    this.listElement = this.$('.quotes');
+
+    // Create a TaskView for each task
+    this.stockList = [];
+    this.stockData.forEach(function(stock) {
+      var card = new QuoteView({
+        stock: stock,
+        template: this.stockTemplate
+      });
+      this.stockList.push(card);
+    }, this);
+
+    // // Keep track of our form input fields
+    // this.input = {
+    //   title: this.$('.new-task input[name="title"]'),
+    //   description: this.$('.new-task input[name="description"]')
+    // };
+  }, // end initialize
+
+
+
 
   render: function() {
-    this.$el.html('<p>Hello World</p>');
+    // Make sure the list in the DOM is empty
+    // before we start appending items
+    this.listElement.empty();
 
-    return this;
-  }
-});
+    // Loop through the data assigned to this view
+    this.stockList.forEach(function(stock) {
+      // Cause the task to render
+      stock.render();
+
+      // Add that HTML to our task list
+      this.listElement.append(stock.$el);
+    }, this);
+
+    return this; // enable chained calls
+  }, // end render
+}); // end Application View
 
 export default ApplicationView;
